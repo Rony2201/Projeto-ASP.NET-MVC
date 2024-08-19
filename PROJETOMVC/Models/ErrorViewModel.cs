5bb7ad9bcbbd5eddaef5523a45c95a84d095bfbc
Rony <roniely_pinheiro@hotmail.com>
2024-08-19 20:01:50 -0300

    projeto mvc

---
+++ PROJETOMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PROJETOMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ PROJETOMVC/Views/Contato/Criar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PROJETOMVC.Views.Contato
+{
+    public class CriarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
